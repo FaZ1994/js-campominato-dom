3988410a15a1987ae15a946b44a17fa6fc2e9a68
Fabrizio Zivolo <faz94@hotmail.it>
2022-04-06 19:42:23 +0200

    logica completata, manca da inserire nel DOM il numerio di tentativi effettuati

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ function getRandomInt(min, max) {
 
 // creo la griglia
 function renderGrid(cellsLength) {
+  const state = {
+    attempts:0,
+    maxAttempts: cellsLength - 16,
+    gameOver:false
+  }
   const columnsLength = Math.ceil(
     Math.sqrt(cellsLength),
     Math.sqrt(cellsLength)
@@ -26,6 +31,7 @@ function renderGrid(cellsLength) {
   numbers.sort(() => 0.5 - Math.random());
 
   */
+  const bombs = createBombs(cellsLength);
 
   const rows = [];
 
@@ -34,7 +40,7 @@ function renderGrid(cellsLength) {
     // creo l'elemento row
     let row = document.createElement("div");
     row.className = "row";
-    const cells = renderCells(columnsLength, rowIndex); // ordinamento progressivo
+    const cells = renderCells(columnsLength, rowIndex, bombs,  state); // ordinamento progressivo
     // const cells = renderCells(columnsLength, numbers); //ordinamento randomico
     row.append(...cells);
     rows.push(row);
@@ -44,7 +50,23 @@ function renderGrid(cellsLength) {
 }
 
 // creo le celle
-function renderCells(columnsLength, rowIndex) {
+function renderCells(columnsLength, rowIndex, bombs, state) {
+  function cellsLogic(index, bombs) {
+    return function onClick() {
+      if (state.gameOver) {
+      }
+      else if (bombs.includes(index)) {
+        this.style.backgroundColor = "red";
+        this.style.backgroundImage = "url(./img/bomb.png)";
+        state.gameOver = true;
+      } else {
+        this.style.backgroundColor = "#6495ed";
+       state.attempts = state.attempts +1;
+       if (state.attempts === state.maxAttempts) {alert("vittoria!")}
+      }
+      console.log(bombs);
+    };
+  }
   //ordinamento progressivo
   // function renderCells(columnsLength, numbers) { //ordinamento randomico
   const cells = [];
@@ -53,17 +75,13 @@ function renderCells(columnsLength, rowIndex) {
     cell.className = "cell";
     cell.innerText = rowIndex * columnsLength + columnIndex + 1; //ordinamento_progressivo
     // cell.innerText = numbers.pop() //ordinamento randomico
-    cell.addEventListener("click", cellsLogic );
+    cell.addEventListener(
+      "click",
+      cellsLogic(rowIndex * columnsLength + columnIndex, bombs)
+    );
     cells.push(cell);
   }
   return cells;
-
-}
-
-function cellsLogic() {/* if(){
-  this.style.backgroundColor = "red";
- 
-} else { */ this.style.backgroundColor = "#6495ed"; //}
 }
 
 document.getElementById("generate").addEventListener("click", function () {
@@ -71,35 +89,20 @@ document.getElementById("generate").addEventListener("click", function () {
   renderGrid(cellsLength);
 });
 
-renderGrid(100);
-
-
-
-
-
-
-
-
-
-
-
-
+renderGrid(17);
 
 // WORK IN PROGRES //
 
-
 // creo array vuoto bombs e imposto il numero massimo di bombe con costante BOMBS_NUM
 
-const bombs= [];
-const BOMBS_NUM = 16;
-
 //funzione che crea le bombe
-function renderBombs (cellsLength) {
-  // creo variabile tentativi massimi con valore di cellsLength - 16 (numero di bombe)
-  maxAttempt = cellsLength - BOMBS_NUM;
+function createBombs(cellsLength) {
+  const bombs = [];
+  const BOMBS_NUM = 16;
   //assegno un numero casuale tra 1 e cellsLength fintanto che bombs.lenght non è minore a BOMBS_NUM
-  while (bombs.lenght < BOMBS_NUM) {
-    let bombsNum = getRandomInt(1, cellsLength);
+  while (bombs.length < BOMBS_NUM) {
+    let bombsNum = getRandomInt(0, cellsLength);
+    console.log(bombsNum);
     // se il numero ottenuto non è già contenuto in bombs[], allora lo pusho
     if (!bombs.includes(bombsNum)) {
       bombs.push(bombsNum);
